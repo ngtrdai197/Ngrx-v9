df87639b6e9b09e1c503a9a7bdfcc12c75343998
Nguyen Dai <ngtrdai290197@gmail.com>
2020-04-25 02:22:07 +0700

    (feat): add redux -> ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { StoreModule } from '@ngrx/store'
+import { RouterModule } from '@angular/router'
+import { EffectsModule } from '@ngrx/effects'
+import { HttpClientModule } from '@angular/common/http'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { BooksFeatureStoreModule } from './store/books/books-store.module'
+import { environment } from '../environments/environment'
+import { appRoutes } from './app.routes'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		RouterModule.forRoot(appRoutes),
+		StoreModule.forRoot({}, {}),
+		BooksFeatureStoreModule,
+		StoreDevtoolsModule.instrument({
+			maxAge: 25,
+			logOnly: environment.production,
+		}),
+		EffectsModule.forRoot([]),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
