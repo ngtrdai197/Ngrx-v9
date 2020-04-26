a59743d6f210a9f4ad781ec51f26a453a14f3cf0
Nguyen Dai <ngtrdai290197@gmail.com>
2020-04-26 15:05:49 +0700

    (chore): move root-store to core module

--- src/@core/core.module.ts
+++ src/@core/core.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
 
+import { BooksFeatureStoreModule } from './store/books/books-store.module'
+import { AuthFeatureStoreModule } from './store/auth/auth-store.module'
+
 @NgModule({
-	declarations: [],
-	imports: [CommonModule, HttpClientModule],
-	exports: [HttpClientModule],
+  declarations: [],
+  imports: [CommonModule, HttpClientModule],
+  exports: [HttpClientModule, BooksFeatureStoreModule, AuthFeatureStoreModule],
 })
 export class CoreModule {}
--- src/@store/index.ts
+++
@@ -1,7 +0,0 @@
-import { BookState } from './books/books.reducer'
-import { AuthState } from './auth/auth.reducer'
-
-export interface AppState {
-  bookState: BookState
-  authState: AuthState
-}
